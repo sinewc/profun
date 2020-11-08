5e2b53116fbecede51a711c416b900e201425f7c
sine <63010872@KMITL.AC.Th>
2020-11-08 16:25:51 +0700

    week11
    week11

---
+++ compare/compare/Source.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MAX_SIZE 100 
+int compare(char* str1, char* str2);
+int main()
+{
+    char str1[MAX_SIZE], str2[MAX_SIZE];
+    int res;
+
+    printf("Enter first string: ");
+    scanf_s("%s", str2,MAX_SIZE);
+    printf("Enter second string: ");
+    scanf_s("%s", str2,MAX_SIZE);
+    res = compare(str1, str2);
+    if (res == 0)
+    {
+        printf("Both strings are equal.");
+    }
+    else if (res < 0)
+    {
+        printf("First string is lexicographically smaller than second.");
+    }
+    else
+    {
+        printf("First string is lexicographically greater than second.");
+    }
+
+    return 0;
+}
+
+int compare(char* str1, char* str2)
+{
+    int i = 0;
+    while (str1[i] == str2[i])
+    {
+        if (str1[i] == '\0' && str2[i] == '\0')
+            break;
+         i++;
+    }
+ return str1[i] - str2[i];
+}
